16c1fbe5845b42c07931183c9914282f41fef60e
Michał Żelazkiewicz <michal.zelazkiewicz@gmail.com>
2023-09-18 14:53:31 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# alpha-vantage-streamlit-demo
+# Streamlit and Alpha Vantage data-driven app example
+
+## Setup
+To run the code in this repository, you need to have Python3 and install dependencies included inside requirements.txt. It is stronly recommended to use the virtual environment for keeping installed packaged from your global Python installation.
+```bash
+python3.10 -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+```
+
+## Running apps
+To run Streamlit app just type the following command:
+```bash
+streamlit run <file_name>.py
+```
+It will open web page containing your application in your browser.
---
+++ stock_time_series.py
@@ -0,0 +1,48 @@
+# Import required libraries
+import streamlit as st
+import pandas as pd
+import requests
+
+API_KEY = 'demo' # Replace it with real value
+
+# Ask user for stock symbol
+symbol = st.text_input('Enter stock symbol:', 'IBM').upper()
+
+# API Endpoint to retrieve Daily Time Series
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+
+# Request the data, parse JSON response and store it in Python variable
+r = requests.get(url, timeout=5)
+data = r.json()
+
+# Extract basic information from collected data
+information = data['Meta Data']['1. Information']
+symbol = data['Meta Data']['2. Symbol']
+last_refreshed = data['Meta Data']['3. Last Refreshed']
+
+# Display the collected data to user using Streamline functions
+st.write('# Alpha Vantage stock price data')
+st.write('## ' + information)
+st.write('### ' + symbol)
+st.write('### Last update: ' + last_refreshed)
+
+st.write('## Time Series (Daily)')
+
+# Use Pandas' Data Frame to prepare data to be displayed in charts
+df = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient='index')
+
+df = df.reset_index()
+df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+
+df['open'] = df['open'].astype(float)
+df['high'] = df['high'].astype(float)
+df['low'] = df['low'].astype(float)
+df['close'] = df['close'].astype(float)
+df['volume'] = df['volume'].astype(int)
+
+df['date'] = pd.to_datetime(df['date'])
+df = df.sort_values(by='date')
+
+# Display Streamline charts
+st.line_chart(df.set_index('date')[['open', 'high', 'low', 'close']])
+st.bar_chart(df.set_index('date')['volume'])
